0034ff5ca77a773c8f702d3f48eddd6c4f9a8b2f
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-06-06 16:33:59 +1000

    Parsing finished, restructured model

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
+# Note that this schema1.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_023101) do
+ActiveRecord::Schema.define(version: 2019_06_06_012501) do
 
   create_table "games", force: :cascade do |t|
-    t.integer "sku"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
